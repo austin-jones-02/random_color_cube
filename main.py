f0058c96a8fd0fd8f9b18669df5245e039419a14
Austin Jones <austinguin0709@gmail.com>
2025-08-14 19:30:36 -0400

    Added button and setup geometry

--- main.py
+++ main.py
@@ -3,3 +3,21 @@ We will need to use tkinter so that we don't have to download any weird or
 new libraries... I just want to use base python
 
 """
+
+from tkinter import *
+
+root = Tk()
+
+root.title("Random color cube")
+root.geometry("500x500")
+
+button = Button(root,
+                text="Hey what's up guys?",
+                cursor="hand2")
+
+button.pack(padx=20, pady=20)
+
+
+
+# Run the program
+root.mainloop()
