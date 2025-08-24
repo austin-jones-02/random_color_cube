60e574756f9365e8418645f6d3735184b5fbfd20
Austin Jones <austinguin0709@gmail.com>
2025-08-24 16:08:13 -0400

    Random color changer now toggles on and off via the button

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 """
-We will need to use tkinter so that we don't have to download any weird or
-new libraries... I just want to use base python
-
-Ooo for the random color what about we make it where it has the full list of 
-color options from the image of all tkinter colors.
-    We could use random.choice... I believe that's the one where you can pick from a list
-        Need to do research on it...
+A Tkinter python script that changes the window color randomly based on the colors
+available from Tkinter.
 
 """
 
@@ -14,30 +9,57 @@ from random import choice
 from tkinter_colors import full_list_of_colors
 
 
+color_changer_on = False
+
 def random_color_chosen():
     """Randomly choses a color from the supported list for Tkinter utilizing random.choice"""
     selected_random_color = choice(full_list_of_colors)
-    print(selected_random_color)
     
     return selected_random_color
 
 
 def color_changer():
-    root.configure(background=random_color_chosen())
-    root.after(1000, color_changer)
+    """Handles the toggle for running the color change or not"""
+    global color_changer_on
+
+    if color_changer_on:
+        # Turn off
+        color_changer_on = False
+        button.config(background='gray98')
+
+    else:
+        # Turn on
+        color_changer_on = True
+        button.config(background='green')
+        run_color_loop()
+
+
+def run_color_loop():
+    """Changes the color of the window background when called"""
+    if color_changer_on:
+        color = random_color_chosen()
+        root.config(background=color)
+        label.config(text=f'Current color: {color}')
+        root.after(500, run_color_loop)
 
 root = Tk()
 
-root.title("Random color cube")
-root.geometry("500x500")
+root.title('Random color cube')
+root.geometry('500x500')
 
 
 button = Button(root,
-                text="Change color of background",
-                cursor="hand2",
-                command=color_changer)
-
+                text='Toggle random color changer',
+                cursor='hand2',
+                command=color_changer
+)
 button.pack(padx=20, pady=20)
+
+label = Label(root,
+              fg='gray',
+              font=('Helvetica', 20),
+)
+label.pack()
 
 
 # Run the program
